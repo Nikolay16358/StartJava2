b860a3a387bf7c585f4c720db0c1768d31ef39f8
Nikolay Titenin <16091969@bk.ru>
2023-08-17 21:16:59 +0300

    implement MyFirstApp

---
+++ Lesson_1/MyFirstApp.java
@@ -0,0 +1,9 @@
+import java.nio.charset.Charset;
+
+public class MyFirstApp {
+    
+    public static void main(String[] args) {
+        System.out.println("Write once, run anywhere!");
+        System.out.println(Charset.defaultCharset().displayName());
+    }
+}
