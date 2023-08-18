b25ed980bdbfc7725edbba648bd1ffe3b5b5dedc
Nikolay16358 <121710925+Nikolay16358@users.noreply.github.com>
2023-08-18 21:13:37 +0300

    Delete "Write once, run anywhere"

--- Lesson_1/MyFirstApp.java
+++ Lesson_1/MyFirstApp.java
@@ -3,7 +3,6 @@ import java.nio.charset.Charset;
 public class MyFirstApp {
     
     public static void main(String[] args) {
-        System.out.println("Write once, run anywhere!");
         System.out.println(Charset.defaultCharset().displayName());
     }
 }
