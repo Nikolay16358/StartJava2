2b94ec1cedcb5712944beac588f79bbbacb3a5ee
Nikolay Titenin <16091969@bk.ru>
2023-10-07 23:13:07 +0300

    implement Calculator and MyFirstGame(HW 1.10)

---
+++ Lesson_1/MyFirstGame.java
@@ -0,0 +1,18 @@
+public class MyFirstGame {
+
+    public static void main(String[] args) {
+        int hiddenNumber = 38;
+        int playerNumber = 48;
+        while (hiddenNumber != playerNumber) {
+            if (playerNumber > hiddenNumber) {
+                System.out.println("Число " + playerNumber + " больше того, что загадал компьютер");
+                playerNumber--;
+            } else {
+                System.out.println("Число " + playerNumber + " меньше того, что загадал компьютер");
+                playerNumber++;
+            }
+        }
+
+        System.out.println("Вы победили!");
+    }
+}
