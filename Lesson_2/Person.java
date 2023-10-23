db9e8594501e47576c522770dddf5c14d24d71d0
Nikolay Titenin <16091969@bk.ru>
2023-10-23 20:27:39 +0300

    implement Person(HW 2.2)

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    
+    String gender = "male";
+    String name = "Jack";
+    float height = 1.87f;
+    float weight = 94f;
+    int age = 26;
+
+    public void go() {
+        System.out.println("Jack is going");
+    }
+
+    public void sit() {
+        System.out.println("Jack is sitting");
+    }
+
+    public void run() {
+        System.out.println("Jack is running");
+    }
+
+    public void talk() {
+        System.out.println("Jack is talking");
+    }
+
+    public void learnJava() {
+        System.out.println("Jack is learning Java");
+    }
+}
