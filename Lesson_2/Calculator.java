9d468bd350cdeb3c1b46554625339bb731ab5c22
Nikolay Titenin <16091969@bk.ru>
2023-10-28 13:50:53 +0300

    modification Calculator (HW 2.6)

---
+++ Lesson_2/Calculator.java
@@ -0,0 +1,46 @@
+public class Calculator {
+
+    private int num1;
+    private int num2;
+    private char sign;
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public void setSign(char sign) {
+        this.sign = sign;
+    }
+
+    public void calculate() {
+        int result = 1;
+        switch (sign) {
+            case '^':
+                for (int i = 1; i <= num2; i++) {
+                    result *= num1;
+                }
+                break;
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            case '%':
+                result = num1 % num2;
+            default:
+                System.out.println("The entered mathematical operation is not supported");
+        }
+        System.out.println("Calculation result: " + num1 + " " + sign + " " + num2 + " = " + result);
+    }
+}
---
+++ Lesson_2/CalculatorTest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Calculator calculator = new Calculator();
+        String userAnswer = "yes";
+        while (!userAnswer.equals("no")) {
+            if (userAnswer.equals("yes")) {
+                System.out.println("Enter the first number: ");
+                calculator.setNum1(scanner.nextInt());
+                System.out.println("Enter the sign of a mathematical operation");
+                calculator.setSign(scanner.next().charAt(0));
+                System.out.println("Enter the second number: ");
+                calculator.setNum2(scanner.nextInt());
+                calculator.calculate();
+                System.out.println("Do you want to continue calculating? [yes/no]: ");
+            } else {
+                System.out.println("Incorrect answer. Enter yes or no");
+            }
+
+            userAnswer = scanner.next();
+        }
+    }
+}
