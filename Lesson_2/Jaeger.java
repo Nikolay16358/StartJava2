58bc915ead5b8b0886c0fc1cc2d255eff21a861a
Nikolay Titenin <16091969@bk.ru>
2023-10-29 21:06:47 +0300

    implement Jaeger (HW 2.6)

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,99 @@
+public class Jaeger {
+
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float weight;
+    private int strength;
+    private int armor;
+
+    public Jaeger() {
+    }
+
+    public Jaeger(String modelName, String mark, String origin, float weight, int strength, int armor) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.origin = origin;
+        this.weight = weight;
+        this.strength = strength;
+        this.armor = armor;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getMark() {
+        return mark;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        if (weight > 0 && weight <= 3_000) {
+            this.weight = weight;
+        } else {
+            System.out.println("The weight value is incorrect");
+        }
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        if (strength > 0) {
+            this.strength = strength;
+        } else {
+            System.out.println("The strength value is incorrect");
+        }
+    }
+
+    public int getArmor() {
+        return armor;
+    }
+
+    public void setArmor(int armor) {
+        if (armor > 0) {
+            this.armor = armor;
+        } else {
+            System.out.println("The armor value is incorrect");
+        }
+    }
+
+    public void killKaiju(int countKills) {
+        System.out.println(modelName + " killed " + countKills + " Kaijus");
+    }
+
+    public void useWeapon() {
+        System.out.println(modelName + " is shooting");
+    }
+
+    public void wasBorn() {
+        if (origin.equals("USA")) {
+            System.out.println("This robot wants to take over the world!");
+        } else if (origin.equals("Russia")) {
+            System.out.println("This robot wants to save the planet!");
+        } else {
+            System.out.println("This robot hasn't made a decision about the destiny of planet");
+        }
+    }
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,39 @@
+public class JaegerTest {
+
+    public static void main(String[] args) {
+        Jaeger jaeger1 = new Jaeger();
+        jaeger1.setModelName("Striker Eureka");
+        jaeger1.setMark("Mark-5");
+        jaeger1.setOrigin("Australia");
+        System.out.println("The first jaeger name is " + jaeger1.getModelName() +
+                ", this robot is from " + jaeger1.getOrigin());
+        jaeger1.setWeight(5000);
+        jaeger1.setWeight(1850);
+        System.out.println("The first robot weight is " + jaeger1.getWeight());
+        jaeger1.setStrength(-3);
+        jaeger1.setStrength(10);
+        System.out.println(jaeger1.getModelName() + " has " + jaeger1.getStrength() + " power");
+        jaeger1.setArmor(-5);
+        jaeger1.setArmor(9);
+        System.out.println(jaeger1.getModelName() + " has " + jaeger1.getArmor() + " armor");
+        jaeger1.killKaiju(11);
+        jaeger1.useWeapon();
+        jaeger1.wasBorn();
+        jaeger1.setOrigin("USA");
+        jaeger1.wasBorn();
+        jaeger1.setOrigin("Russia");
+        jaeger1.wasBorn();
+
+        Jaeger jaeger2 = new Jaeger("Cherno Alpha", "Mark-1", "Russia", 2412f, 10, 10);
+        System.out.println("\nThe second jaeger name is " + jaeger2.getModelName() +
+                ", this robot is from " + jaeger2.getOrigin() + ", his weight is " + jaeger2.getWeight() +
+                ", he has " + jaeger2.getStrength() + " power and " + jaeger2.getArmor() + " armor");
+        jaeger2.wasBorn();
+        jaeger2.killKaiju(6);
+        jaeger2.useWeapon();
+        jaeger2.setOrigin("China");
+        jaeger2.wasBorn();
+        jaeger2.setOrigin("Russia");
+        jaeger2.wasBorn();
+    }
+}
