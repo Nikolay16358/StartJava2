60537c79ec1be6ff68e6592fc9ba292df6cacffa
Nikolay Titenin <16091969@bk.ru>
2023-10-30 18:22:28 +0300

    correction MyFirstGame №1 (HW 2.6)

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -6,6 +6,7 @@ public class GuessNumber {
 
     private Player player1;
     private Player player2;
+    private static int hiddenNumber;
 
     public GuessNumber(Player player1, Player player2) {
         this.player1 = player1;
@@ -13,30 +14,29 @@ public class GuessNumber {
     }
 
     public void play() {
-        int hiddenNumber = (int) (Math.random() * 100 + 1);
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            System.out.println(player1.getName() + ", enter a number: ");
-            player1.setNumber(scanner.nextInt());
-            if (hiddenNumber > player1.getNumber()) {
-                System.out.println("The number " + player1.getNumber() + " is less than the computer thought.");
-            } else if (hiddenNumber < player1.getNumber()) {
-                System.out.println("The number " + player1.getNumber() + " is more than the computer thought.");
-            } else {
-                System.out.println(player1.getName() + ", You are the winner!");
+        hiddenNumber = (int) (Math.random() * 100 + 1);
+        while (hiddenNumber != player2.getNumber()) {
+            startGame(player1);
+            if (hiddenNumber == player1.getNumber()) {
                 break;
             }
+            startGame(player2);
+        }
 
-            System.out.println(player2.getName() + ", enter a number: ");
-            player2.setNumber(scanner.nextInt());
-            if (hiddenNumber > player2.getNumber()) {
-                System.out.println("The number " + player2.getNumber() + " is less than the computer thought.");
-            } else if (hiddenNumber < player2.getNumber()) {
-                System.out.println("The number " + player2.getNumber() + " is more than the computer thought.");
-            } else {
-                System.out.println(player2.getName() + ", You are the winner!");
-                break;
-            }
+        player1.setNumber(0);
+        player2.setNumber(0);
+    }
+
+    private void startGame(Player player) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(player.getName() + ", enter a number: ");
+        player.setNumber(scanner.nextInt());
+        if (hiddenNumber > player.getNumber()) {
+            System.out.println("The number " + player.getNumber() + " is less than the computer thought.");
+        } else if (hiddenNumber < player.getNumber()) {
+            System.out.println("The number " + player.getNumber() + " is more than the computer thought.");
+        } else {
+            System.out.println(player.getName() + ", You are the winner!");
         }
     }
 }
